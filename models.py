c4dbb9ac219dd62fdfae5923034fd6ec9185b6b8
Gwali <gwalisam37@gmail.com>
2023-04-12 18:04:01 +0000

    setting up models

--- models.py
+++ models.py
@@ -1,1 +1,76 @@
-#dbmodels
+from sqlalchemy.sql import column
+from .database import Base
+from sqlalchemy import ForeignKeyConstraint, String, Integer, Boolean, Column, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    email = Column(String, index=True)
+    password = Column(String)
+    profile = relationship("Profile", back_populates="user")
+    buckets = relationship("UserBucket", back_populates="user")
+    songs = relationship("Songs")
+    
+
+
+class Profile(Base):
+    __tablename__ = "profile"
+    id = Column(Integer, primary_key=True, index=True)
+    contributions = Column(Integer)
+    playlist_exports = Column(Integer)
+    created_buckets = Column(Integer)
+    music_tatse = Column(Integer)
+    sportify_profile = Column(String)
+
+    
+
+class Songs(Base):
+    __tablename__ = "songs"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    artist_name = Column(String)
+    cover_art_url = Column(String)
+    preview_url = Column(String)
+    song_duration = Column(String)
+    external_url = Column(String)
+    
+
+
+
+class Bucket(Base):
+    __tablename__ = "bucket"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    number_of_tracks = Column(Integer)
+    created_at = Column(DateTime, index=True)
+    cover_art_url = Column(String)
+    song = relationship("Songs")
+
+
+
+class UserBucket(Base):
+    __tablename__ = "bucket"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    number_of_tracks = Column(Integer)
+    created_at = Column(DateTime, index=True)
+    cover_art_url = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    song_id= Column(Integer, ForeignKey("songs.id"))
+    songs = relationship("Songs")<Right>
+ 
+
+
+
+
+
+
+
+
+
+
