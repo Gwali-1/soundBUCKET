f82fed80821cb3f4430691b984e4d38170eea106
Gwali <gwalisam37@gmail.com>
2023-04-21 12:21:24 +0000

    user token info gets saved to db

--- routers/sportify_operations.py
+++ routers/sportify_operations.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends 
+from fastapi import APIRouter, Depends, HTTPException 
 from fastapi.responses import RedirectResponse, JSONResponse
 from .utility import get_auth_object , cache_handler
 from ..dependencies import get_token_header
@@ -11,7 +11,7 @@ router = APIRouter(
 
 #collect auth code and return it 
 @router.get("/callback")
-async def sport_home(code:str): 
+def sport_home(code:str): 
     if not code:
         data = {"message":"error, something went wrong"}
         return JSONResponse(content=data, status_code= 400) 
@@ -24,22 +24,28 @@ async def sport_home(code:str):
 
 #get token info and save 
 @router.post("/add_token_info")
-async def add_token(code:schema.Code, user_id:int = Depends(get_token_header)):
+def add_token(code:schema.Code, user_id:int = Depends(get_token_header)):
     auth_object = get_auth_object()
     token_info = auth_object.get_access_token(code)
-    await cache_handler(user_id, token_info)
-    data = {"message":"Successful Authorized"}
-    return JSONResponse(content=data, status_code=200)
+    if token_info:
+        try:
+            cache_handler(user_id, token_info)
+            data = {"message":"Successful Authorized"}
+            return JSONResponse(content=data, status_code=200)
+
+        except:
+            raise HTTPException(status_code=401, detail="could not authorize at the moment")
+
+    raise HTTPException(status_code=401, detail="could not authorize at the moment")
 
 
 
 #spotofy auth route 
 @router.get("/authenticate")
-async def authorize_spotify():
+def authorize_spotify():
     #check if user has token 
     auth_object = get_auth_object()
     url = auth_object.get_authorize_url()
-    print(url)
     return RedirectResponse(url)
 
 
